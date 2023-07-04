ab686b4558905d5c972f65fe40760c9a824d9142
Jigisha Manohar <jigishamanohar18@gmail.com>
2023-07-05 01:27:42 +0530

    contact functionality

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 import './Contact.css'
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_ien3edf',
+        'template_t14wsxu',
+        form.current,
+        'ZFF2ensoiVlBKbQoc'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          console.log('message sent')
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+    e.target.reset()
+  }
+
   return (
     <div className="contact">
       <div className="contact-text">
         <h1>I'd love to hear from you . . .</h1>
       </div>
-      {/* <div className="wrap">
-        <div className="contact-one divs">
-          Wanna Collaborate? <br />
-          <button className="text">Mail Me!</button>
-        </div>
-        <div className="contact-two divs">
-          Wanna know more about
-          <br /> <button className="text">Mail Me!</button>
-        </div>
-        <div className="contact-three divs">
-          Contact Me
-          <br />
-          <button className="text">Mail Me!</button>
-        </div>
-      </div> */}
 
       <div className="wrap">
         <div className="contact-center"></div>
-        <form action="">
+        <form
+          action=""
+          ref={form}
+          onSubmit={sendEmail}
+        >
           <div className="row">
             <div className="input-group">
               <input
                 type="text"
                 id="name"
+                name="user_name"
                 required
               />
               <label htmlFor="name">Name</label>
@@ -40,6 +55,7 @@ const Contact = () => {
               <input
                 type="number"
                 id="contact"
+                name="user_contact"
                 required
               />
               <label htmlFor="contact">Contact</label>
@@ -49,6 +65,7 @@ const Contact = () => {
             <input
               type="email"
               id="email"
+              name="user_email"
               required
             />
             <label htmlFor="email">Email</label>
@@ -57,11 +74,17 @@ const Contact = () => {
             <textarea
               rows="8"
               id="message"
+              name="user_message"
               required
             ></textarea>
             <label htmlFor="message">Message</label>
           </div>
-          <button type="submit">Submit</button>
+          <button
+            type="submit"
+            value="Send"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
