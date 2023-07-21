e5394098f094fc854e4211df3710b7f2ae692124
Jigisha Manohar <jigishamanohar18@gmail.com>
2023-07-21 17:59:19 +0530

    project addition

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,6 +5,8 @@ import blogs from '../../assets/blogs.png'
 import portfolio from '../../assets/portfolio.png'
 import food from '../../assets/food.png'
 import login from '../../assets/login.png'
+import xzect from '../../assets/xzect.png'
+import dairy from '../../assets/dairy.png'
 
 import Flipcard from '../../components/Flipcard/Flipcard'
 
@@ -13,39 +15,45 @@ import './Projects.css'
 const Projects = () => {
   const data = [
     {
-      id: 1,
       image: dental,
       title: 'Dental Clinic Management System',
       text: 'It is a full stack webapp made using Django for backend and database and Bootstrap for frontend. It has user authentication, make and manage applications, get location, etc.',
       link: 'https://github.com/Jigisha15/DentalClinic',
     },
     {
-      id: 2,
       image: blogs,
       title: 'Blogging App',
       text: 'This is a Blogging App made using Django. With this app you can post and view blogs. It has user authentication added.',
       link: 'https://github.com/Jigisha15/Blogs',
     },
     {
-      id: 3,
+      image: dairy,
+      title: 'Dairy Management App',
+      text: 'This is my first Django project that I made with the help of a YouTube tutorial of which the link is provided in my GitHub ReadMe.',
+    },
+    {
       image: food,
       title: 'Food Ordering Website',
       text: 'This is a full stack website made using ReactJS. It has planned features like user authentication, contact the admin, Order online along with a user-friendly UI.',
       link: 'https://github.com/Jigisha15/Food',
     },
     {
-      id: 4,
       image: portfolio,
       title: 'My Portfolio',
       text: 'This is my Portfolio made using ReactJs. You can share the link and contact me!',
       link: 'https://github.com/Jigisha15/Portfolio',
     },
     {
-      id: 5,
       image: login,
       title: 'UI for Login & Registeration',
       text: 'This is a small UI made using vanilla HTML,CSS in ReactJS framework. You can use this for adding a login and registeration page in your website along with custom images.',
       link: 'https://github.com/Jigisha15/Clone',
+    },
+    {
+      image: xzect,
+      title: 'Clone Webiste',
+      text: 'This is a clone of an existing webiste to kickstart my journey in NextJS framework. The styling is entirely done using CSS.',
+      link: 'https://github.com/Jigisha15/Xzect',
     },
   ]
 
@@ -55,10 +63,10 @@ const Projects = () => {
     <div className="container">
       <h1 className="outer-text">Here are my Projects</h1>
       <div className="outer">
-        {data.map((item) => (
+        {data.map((item, index) => (
           <div
             className="project"
-            key={item.id}
+            key={index}
           >
             <Flipcard
               image={item.image}
